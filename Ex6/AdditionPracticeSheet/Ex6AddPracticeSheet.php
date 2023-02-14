4784ce5c7ca7047202b8d83188d124e9b0110c8b
mask_detection <mask_detection@leedsand.com.tw>
2023-02-14 15:28:06 +0800

    Ex6-1

---
+++ Ex6/AdditionPracticeSheet/Ex6AddPracticeSheet.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>加法練習表</title>
+</head>
+<body>
+加法練習表
+<p>
+<form  name="form1" method="POST" action="Ex6Add1.php">
+<input type="number" name="n1">+<input type="number" name="n2">
+<input type="submit" name="Submit" value="確定">
+</form>
+</p>
+</body>
+</html>
