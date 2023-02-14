0d07b8633f9548a87f654c6a164cf7f2960994da
mask_detection <mask_detection@leedsand.com.tw>
2023-02-14 15:28:06 +0800

    SessionPHP7

--- Ch06/sessionpassword/TestCh6-2-1.php
+++ Ch06/sessionpassword/TestCh6-2-1.php
@@ -1,32 +1,4 @@
 <?php
-if (!function_exists('session_is_registered')) {
-    function session_is_registered($name) {
-        if (isset($_SESSION[$name])) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-}
- 
-if (!function_exists('session_register')) {
-    function session_register() {
-        $args = func_get_args();
-        foreach ($args as $key) {
-            $_SESSION[$key] = $GLOBALS[$key];
-        }
-    }
-}
- 
-if (!function_exists('session_unregister')) {
-    function session_unregister($name) {
-        unset($_SESSION[$name]);
-    }
-}?>
-
-
-
-<?php
 session_start();
 $name=$_POST["name"];
 $password=$_POST["password"];
@@ -35,5 +7,6 @@ if($name !="test" || $password !="1234")
     echo "帳號或密碼輸入錯誤";
     exit;
 }
-session_register("password","name");
+$_SESSION["password"]=$password;
+$_SESSION["name"]=$name;
 ?>
