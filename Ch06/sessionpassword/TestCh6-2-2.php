4b7d6d71f6db9c32c53feb41d3c0c8e20b4f27b4
mask_detection <mask_detection@leedsand.com.tw>
2023-02-14 15:21:10 +0800

    sessionpassword_Test(register)

---
+++ Ch06/sessionpassword/TestCh6-2-2.php
@@ -0,0 +1,18 @@
+<?php
+require("TestCh6-2-1.php"); //引入檢查密碼的檔案
+?>
+<?php
+echo $_SESSION["name"]."您好!<p>";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>會員使用專區</title>
+</head>
+<body>
+會員使用專區
+</body>
+</html>
