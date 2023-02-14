b1069d16787abfebef067c8b161755487b8479d7
mask_detection <mask_detection@leedsand.com.tw>
2023-02-14 11:11:07 +0800

    nl2br

---
+++ Ch06/nl2br/TestCh6-1-2.php
@@ -0,0 +1,3 @@
+<?php
+echo "未使用nl2br函數:".$_POST["body"]."<p>";
+echo "使用nl2br函數:".nl2br($_POST["body"]);
---
+++ Ch06/nl2br/TestCh6-1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>範例練習</title>
+</head>
+<body>
+    <form name="form1" method="POST" action="TestCh6-1-2.php">
+        請輸入留言內容:<br><textarea name="body" rows=10 cols=40></textarea>
+        <input type="submit" name="Submit" value="送出"> 
+    </form>
+</body>
+</html>
