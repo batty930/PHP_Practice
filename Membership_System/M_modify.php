291bcc09b07c4dbb3cb33d35e2b4e53d5fa26d35
mask_detection <mask_detection@leedsand.com.tw>
2023-02-23 17:16:03 +0800

    0223更改update寫法

--- Membership_System/M_modify.php
+++ Membership_System/M_modify.php
@@ -3,12 +3,11 @@ session_start();
 header("location:M_view.php");
 include("M_db.php");
 
-$email=$_POST["email"];
-$address=$_POST["address"];
-$tel=$_POST["tel"];
-$id=$_SESSION["user"];
-
-$sql="UPDATE user SET email='$email',address='$address',tel='$tel' WHERE id='$id'";
+$email = $_POST["email"];
+$address = $_POST["address"];
+$tel = $_POST["tel"];
+$id = $_SESSION["user"];
+$data = ["$email", "$address", "$tel"];
+$sql = "UPDATE user SET (email,address,tel) =(?,?,?)WHERE id='$id'";
 $update = $conn->prepare($sql);
 $update->execute();
-?>
